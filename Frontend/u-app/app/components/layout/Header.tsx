5ef671364ad83c560cd1f8cdd912aca9334e55a7
Fabio Romero <romero.gomez.fabio@gmail.com>
2025-03-21 01:39:35 -0500

    feat: add admin button

--- Frontend/u-app/app/components/layout/Header.tsx
+++ Frontend/u-app/app/components/layout/Header.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button"
-import { Sun, Moon, Volume2, LogOut } from "lucide-react"
-import { Link } from "lucide-react"
+import { Sun, Moon, Volume2, LogOut, ShieldAlert } from "lucide-react"
+import Link from "next/link"
 import ProgressStats from "./ProgressStats"
+import { useAuth } from "@/app/context/AuthContext"
 
 interface HeaderProps {
   theme: string
@@ -10,14 +11,30 @@ interface HeaderProps {
 }
 
 export default function Header({ theme, toggleTheme, handleLogout }: HeaderProps) {
+  const {user} =useAuth()
+  console.log(user)
   return (
       <div className="sticky top-0 z-50">
         <header className="bg-white dark:bg-wayuu-dark-card shadow-md">
           <div className="container mx-auto px-4 py-3">
             <div className="flex justify-between items-center">
+            
+            <Link href="/pages/home">
               <h1 className="text-3xl font-bold font-display text-wayuu-red">YU</h1>
-
+            </Link>
               <div className="flex space-x-2">
+                {
+                  (user?.role.name == "admin") &&
+                  <Link href="/pages/admin">
+                    <Button
+                      variant="outline"
+                      className="bg-wayuu-red text-white hover:bg-wayuu-red/80 dark:bg-wayuu-red dark:text-white dark:hover:bg-wayuu-red/80 flex items-center gap-2"
+                      >
+                      <ShieldAlert className="h-4 w-4" />
+                      <span className="hidden sm:inline">Panel Admin</span>
+                    </Button>
+                  </Link>
+                } 
                 <Button
                   variant="outline"
                   size="icon"
